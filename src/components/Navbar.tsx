1b0dcb4a1b366f0a630a22cd12b3c2f4a3bda003
Sacha <sacha.alt@proton.me>
2025-04-01 21:13:39 +0200

    improved hero and navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,36 +5,36 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Navbar() {
 	return (
-		<div className='fixed top-0 w-screen justify-center flex z-1'>
-		
-			<div>
-				<div className='flex w-full justify-center p-4'>
-					<nav className='  backdrop-blur-xl bg-white/5 rounded-2xl shadow-[0_8px_32px_0_rgba(231,238,235,0.2)] border border-white/20 flex items-center justify-between px-8 py-4 w-auto'>
-						<div className='flex items-center gap-x-8 gap-y-3'>
-							<div className='flex items-center gap-x-6 gap-y-2'>
-								<Link
-									href='/features'
-									className='text-white/50 hover:text-white transition-colors font-mono'
-								>
-									Portfolio
-								</Link>
-								<Link
-									href='/pricing'
-									className='text-white/50 hover:text-white transition-colors font-mono'
-								>
-									Photography
-								</Link>
-								<Link
-									href='/about'
-									className='text-white/50 hover:text-white transition-colors font-mono'
-								>
-									Music
-								</Link>
-							</div>
-						</div>
-					</nav>
+		<div className='fixed top-0 w-screen z-1'>
+			<div className='flex w-full justify-between p-4'>
+				<div className='hidden md:inline font-mono text-2xl self-center'>
+					sachaa.fr
 				</div>
-				<div className='flex space-x-8'>
+				<nav className='backdrop-blur-xl bg-white/5 rounded-2xl shadow-[0_8px_32px_0_rgba(231,238,235,0.2)] border border-white/20 flex items-center justify-between px-8 py-4 w-full sm:w-auto'>
+					<div className='flex items-center gap-x-8 gap-y-3'>
+						<div className='flex items-center gap-x-6 gap-y-2'>
+							<Link
+								href='/features'
+								className='text-white/50 hover:text-white transition-colors font-mono'
+							>
+								Home
+							</Link>
+							<Link
+								href='/pricing'
+								className='text-white/50 hover:text-white transition-colors font-mono'
+							>
+								Photography
+							</Link>
+							<Link
+								href='/about'
+								className='text-white/50 hover:text-white transition-colors font-mono'
+							>
+								Music
+							</Link>
+						</div>
+					</div>
+				</nav>
+				<div className='hidden md:flex self-center space-x-8 '>
 					<Link
 						href='https://github.com/Blackers33'
 						target='_blank'
